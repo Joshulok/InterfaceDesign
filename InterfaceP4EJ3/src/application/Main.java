8855bb8fd887b61916eaf80b6b781e4c72bb3758
Joshulok <joshuarojasgonzalez@MBP-de-Joshua.home>
2021-10-27 11:07:12 +0200

    problemas con scene builder

--- InterfaceP4EJ3/src/application/Main.java
+++ InterfaceP4EJ3/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("View.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Vista.fxml"));
 			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
